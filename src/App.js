e8bd72d6b399c6fc7985cce6663989b31f70af01
LAPT-CSH-23 <cshtd.computacion@gmail.com>
2024-10-29 15:49:32 -0600

    Se ha implementado el navbar mediante bootstrap y se ha añadido las rutas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import RecipeCard from "./components/RecipeCard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
-  
+
   // search for the recipe
   const searchRecipes = async () => {
     setIsLoading(true);
@@ -29,22 +30,30 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h2>¡Bienvenido a Food Galaxy!</h2>
-      <SearchBar
-        isLoading={isLoading}
-        query={query}
-        setQuery={setQuery}
-        handleSubmit={handleSubmit}
-      />
-      <div className="recipes">
-        
-        {recipes ? recipes.map(recipe => (
-          <RecipeCard
-             key={recipe.idMeal}
-             recipe={recipe}
-          />
-        )) : "No Results."}
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<></>} />
+        </Routes>
+      </BrowserRouter>
+      <div className="container">
+        <h2>¡Bienvenido a Food Galaxy!</h2>
+        <SearchBar
+          isLoading={isLoading}
+          query={query}
+          setQuery={setQuery}
+          handleSubmit={handleSubmit}
+        />
+        <div className="recipes">
+
+          {recipes ? recipes.map(recipe => (
+            <RecipeCard
+              key={recipe.idMeal}
+              recipe={recipe}
+            />
+          )) : "No Results."}
+        </div>
       </div>
     </div>
   );
